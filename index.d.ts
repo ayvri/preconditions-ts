cd5bc788cd16066e0c77cd0ff008f4ff5cd58838
Damon Oehlman <damon.oehlman@gmail.com>
2018-12-23 12:18:13 +1100

    no reason to commit d.ts files

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-import { DataSource } from './core';
-export declare const optionalNumber: (d: DataSource, key: string) => number | null | undefined;
-export declare const optionalString: (d: DataSource, key: string) => string | null | undefined;
-export declare const optionalBoolean: (d: DataSource, key: string) => boolean | null | undefined;
-export declare const optionalArray: (d: DataSource, key: string) => [] | null | undefined;
-export declare const requiredNumber: (d: DataSource, key: string) => number;
-export declare const requiredString: (d: DataSource, key: string) => string;
-export declare const requiredBoolean: (d: DataSource, key: string) => boolean;
-export declare const requiredArray: (d: DataSource, key: string) => [];
